31c2fe5de21f6b3a79748c8ed2655caa43f894fd
JohnOkari <johnnokari@gmail.com>
2022-12-01 19:36:15 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function () {
+  //DOM elements
+  const newTaskForm = document.getElementById("create-task-form");
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+const createNewTask = function (e) {
+  e.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  e.target.reset();
+};
+
+const appendNewTask = (task) => {
+  document.getElementById("tasks").appendChild(task);
+};
